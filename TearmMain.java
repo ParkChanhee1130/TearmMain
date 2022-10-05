01934d3e0c1dfda25e53c88f69eb7224113534df
ParkChanhee1130 <kkazza1130@gmail.com>
2022-10-05 15:17:59 +0900

    본인 이름

---
+++ TearmMain.java
@@ -0,0 +1,10 @@
+public class TearmMain {
+    public static void main() {
+        double d = 120.0;
+        int i = 100;
+        int result = i;
+
+        System.out.println("d = " + d);
+        return;
+    }
+}
